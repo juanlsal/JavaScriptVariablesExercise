ea6edfafbf9a0f28182d4ebf46f271adeb509341
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-13 10:54:15 -0500

    pushed to repo

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age
 // Exercise 2
-
+const firstName = "Juan"
+age = 22;
+lastName = "Lopez Salamanca"
 // Exercise 3
+let language = "JavaScript"
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was first released in ${createdYear}`);
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M"
+// Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
